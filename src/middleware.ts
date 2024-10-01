2b52e09e4187726be82ad7ae1b8d928fc67bf83d
charankamal20 <charanksingh20@gmail.com>
2024-10-02 00:21:21 +0530

    Fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,9 @@ import type { NextRequest } from "next/server";
 const publicPaths = ["/auth/login", "/auth/register", "/"];
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("jwt-token")?.value;
-  console.log("PATHNAME: ", request.nextUrl.pathname);
+  const token = request.cookies.get("auth_token")?.value;
 
-  let pathname = request.nextUrl.pathname;
+  const pathname = request.nextUrl.pathname;
   if (
     publicPaths.includes(request.nextUrl.pathname) ||
     pathname.startsWith("/user/verify")
@@ -17,6 +16,7 @@ export function middleware(request: NextRequest) {
   }
 
   if (!token) {
+    console.log("TOKEN NOT FOUND")
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 
