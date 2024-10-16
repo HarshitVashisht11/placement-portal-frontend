cb24d78d130cd7049b6f8dc6f495a0c594b4dae0
charankamal20 <charanksingh20@gmail.com>
2024-10-17 01:42:18 +0530

    Change registration to otp login

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const publicPaths = ["/auth/login", "/auth/register", "/"];
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("auth_token")?.value;
+  if(!token) {
+    console.log("TOKEN NOT FOUND")
+  }
+  const pathname = request.nextUrl.pathname;
+
+  if (
+    publicPaths.includes(request.nextUrl.pathname) ||
+    pathname.startsWith("/user/verify")
+  ) {
+    return NextResponse.next();
+  }
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/((?!_next|favicon.ico).*)",
+};
--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -3,13 +3,6 @@ import * as z from "zod";
 export const RegisterSchema = z.object({
   name: z.string().min(1, "Name is required").optional(),
   email: z.string().email("Invalid email address").optional(),
-  password: z
-    .string()
-    .min(1, "Password is required")
-    .regex(
-      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-      "Password must be at least 8 characters long, containing both letters and numbers."
-    ).optional(),
   rollnum: z
     .string()
     .regex(/^\d[1-9][3512]\d{2}$/, "Please Enter a Valid Roll Number."),
