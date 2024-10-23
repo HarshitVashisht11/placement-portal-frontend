30e6a03da489dca9dba97dd019576f95bfd7cf33
charankamal20 <charanksingh20@gmail.com>
2024-10-23 23:09:05 +0530

    Draft Commit: Integrate Dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,51 +5,63 @@ import { DM_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/layout/Navbar";
 import { Toaster } from "react-hot-toast";
+import NextTopLoader from "nextjs-toploader";
 
 const fontHeading = DM_Sans({
-    subsets: ["latin"],
-    display: "swap",
-    variable: "--font-heading",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-heading",
 });
 
 const fontBody = DM_Sans({
-    subsets: ["latin"],
-    display: "swap",
-    variable: "--font-body",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-body",
 });
 
 const dmsans = DM_Sans({
-    subsets: ["latin"],
-    weight: ["300", "400", "500", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
-    title: "Placement Portal",
-    description: "TPC",
+  title: "Placement Portal",
+  description: "TPC",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html suppressHydrationWarning lang="en">
-            <body
-                className={cn(
-                    fontHeading.variable,
-                    fontBody.variable,
-                    dmsans.className
-                )}
-            >
-                <div
-                    className={`relative gap-8 max-w-screen-xl mx-auto flex flex-col px-2 justify-start min-h-screen`}
-                >
-                    <Navbar />
-                    <main className="">{children}</main>
-                    <Toaster position="bottom-center" />
-                </div>
-            </body>
-        </html>
-    );
+  return (
+    <html suppressHydrationWarning lang="en">
+      <body
+        className={cn(
+          fontHeading.variable,
+          fontBody.variable,
+          dmsans.className
+        )}
+      >
+        <div
+          className={`relative gap-8 max-w-screen-xl mx-auto flex flex-col px-2 justify-start min-h-screen`}
+        >
+          <NextTopLoader
+            color="#2299DD"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          />
+          <Navbar />
+          <main className="">{children}</main>
+          <Toaster position="bottom-center" />
+        </div>
+      </body>
+    </html>
+  );
 }
